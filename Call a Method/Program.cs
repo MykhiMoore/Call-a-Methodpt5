28c2e52e9f5bb7213ee4b51cb11204bd5245a072
MykhiMoore <moore_mykhi@student.mahoningctc.com>
2023-11-01 11:14:03 -0400

    Add project files.

---
+++ Call a Method/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CallAMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string msg = "Yabba dabba doo!";
+
+            Math.Min(3, 5);
+
+            Console.WriteLine(msg);
+
+            Console.WriteLine(msg.Substring(0, 1));
+        }
+    }
+}
